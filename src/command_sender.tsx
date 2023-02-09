219e2896c0a3ee0954f742d8f08548d9a72985f4
Ivan Kruglov <kruglovivan005@gmail.com>
2023-02-09 13:56:58 +0300

    added command selector styles

--- src/command_sender.tsx
+++ src/command_sender.tsx
@@ -1,10 +1,14 @@
-export default function CommandForm({corpus, cabinet, computer}: {corpus: string, cabinet: string, computer: string | undefined}) {
+export default function CommandForm({
+                                        corpus,
+                                        cabinet,
+                                        computer
+                                    }: { corpus: string, cabinet: string, computer: string | undefined }) {
     const sendCommand = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         const other_data = e.currentTarget.other_data.value
         const command = e.currentTarget.command.value
 
-        fetch(`/api/computer/${corpus}/${cabinet}${computer? ("/" + computer) : ""}`, {
+        fetch(`/api/computer/${corpus}/${cabinet}${computer ? ("/" + computer) : ""}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -28,21 +32,62 @@ export default function CommandForm({corpus, cabinet, computer}: {corpus: string
 
     return (
         <form onSubmit={sendCommand}>
-            <select name={"command"} onChange={setCommand} defaultValue={"ffx"}>
-                <option value={"shd"}>Shutdown</option>
-                <option value={"rbt"}>Reboot</option>
-                <option value={"blc"}>Lock</option>
-                <option value={"ubl"}>Unlock</option>
-                <option value={"ffx"}>Open Firefox</option>
-                <option value={"ntf"}>Send message</option>
-                <option value={"pau"}>Enable change wifi</option>
-                <option value={"pno"}>Disable change wifi</option>
-                <option value={"los"}>Logout student</option>
-                <option value={"est"}>Enable system settings</option>
-                <option value={"dst"}>Disable system settings</option>
-            </select>
-            <input name={"other_data"} required={true} hidden={false}/>
-            <button>Send</button>
+            <div style={
+                {
+                    width: "700px",
+                    alignItems: "center",
+                }
+            }>
+                <div style={
+                    {
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                        flexWrap: "wrap",
+                        gap: "1rem",
+                        padding: "20px",
+                        maxWidth: "500px",
+                        border: "3px solid #ccc",
+                        borderRadius: "10px",
+                        overflow: "hidden"
+                    }
+                }>
+                    <select name={"command"} onChange={setCommand} defaultValue={"ffx"} style={
+                        {
+                            width: "100%",
+                            maxWidth: "500px",
+                            height: "30px",
+                        }
+                    }>
+                        <option value={"shd"}>Shutdown</option>
+                        <option value={"rbt"}>Reboot</option>
+                        <option value={"blc"}>Lock</option>
+                        <option value={"ubl"}>Unlock</option>
+                        <option value={"ffx"}>Open Firefox</option>
+                        <option value={"ntf"}>Send message</option>
+                        <option value={"pau"}>Enable change wifi</option>
+                        <option value={"pno"}>Disable change wifi</option>
+                        <option value={"los"}>Logout student</option>
+                        <option value={"est"}>Enable system settings</option>
+                        <option value={"dst"}>Disable system settings</option>
+                    </select>
+                    <input name={"other_data"} required={true} hidden={false} style={
+                        {
+                            width: "100%",
+                            maxWidth: "500px",
+                            height: "30px",
+                        }
+                    }/>
+                    <button style={
+                        {
+                            width: "100%",
+                            maxWidth: "500px",
+                            height: "30px",
+                        }
+                    }>Send
+                    </button>
+                </div>
+            </div>
         </form>
     )
 }
